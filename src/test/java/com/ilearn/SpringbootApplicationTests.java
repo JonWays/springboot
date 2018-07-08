c66b78f5af624048361322f58fb48c213d7f217a
JonWays <zhuweijun110>
2018-07-08 23:07:41 +0800

    简单增删改查的Demo

--- src/test/java/com/ilearn/SpringbootApplicationTests.java
+++ src/test/java/com/ilearn/SpringbootApplicationTests.java
@@ -1,11 +1,14 @@
-package com.ilearn.springboot;
+package com.ilearn;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class) @SpringBootTest public class SpringbootApplicationTests
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApplicationTests
 {
 
     @Test
